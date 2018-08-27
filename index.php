9f7d1468211d490343c70924b40599091b0e0ecc
Smbat <smbat.aghekyan@gmail.com>
2018-08-27 21:30:39 +0400

    first commit

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
+
 <?php
 
-define('WEBROOT', str_replace("Webroot/index.php", "", $_SERVER["SCRIPT_NAME"]));
-define('ROOT', str_replace("Webroot/index.php", "", $_SERVER["SCRIPT_FILENAME"]));
+define('WEBROOT', str_replace("index.php", "", $_SERVER["SCRIPT_NAME"]));
+define('ROOT', str_replace("index.php", "", $_SERVER["SCRIPT_FILENAME"]));
 
 require(ROOT . 'Config/core.php');
 
