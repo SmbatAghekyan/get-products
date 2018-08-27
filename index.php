cdf2899a875893378a392a5f41ab7284b4fd87d1
SmbatAghekyan <37499445+SmbatAghekyan@users.noreply.github.com>
2018-08-27 21:49:16 +0400

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 define('WEBROOT', str_replace("index.php", "", $_SERVER["SCRIPT_NAME"]));
 define('ROOT', str_replace("index.php", "", $_SERVER["SCRIPT_FILENAME"]));
-echo"<pre>"; print_r(ROOT); echo "</pre>"; exit;
+echo"<pre>"; print_r($_SERVER); echo "</pre>"; exit;
 require(ROOT . 'Config/core.php');
 require(ROOT . 'router.php');
 require(ROOT . 'request.php');
