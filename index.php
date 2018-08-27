852ac79ada5eb91e8ebb6c6fc2c265e2803e2eea
SmbatAghekyan <37499445+SmbatAghekyan@users.noreply.github.com>
2018-08-27 22:36:38 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 
 <?php
 
+
+if(isset($_GET['devel']) && $_GET['devel'] == 'true'){
+	
+	error_reporting( E_ALL );
+	ini_set('display_errors', '1');
+}
 define('WEBROOT', str_replace("index.php", "", $_SERVER["SCRIPT_NAME"]));
 define('ROOT', dirname(__file__).DIRECTORY_SEPARATOR);
 
