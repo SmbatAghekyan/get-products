dc92009aa9c00e122587aafa49d8395974308cd9
SmbatAghekyan <37499445+SmbatAghekyan@users.noreply.github.com>
2018-08-27 22:26:25 +0400

    Update request.php

--- request.php
+++ request.php
@@ -5,6 +5,7 @@
 
         public function __construct()
         {
+            echo"<pre>"; print_r($_SERVER); echo "</pre>"; exit;
             $this->url = $_SERVER["REQUEST_URI"];
         }
     }
